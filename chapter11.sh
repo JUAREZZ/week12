f8d5ff4d3389461cb5fe567a9a72e96986981ac3
JUAREZZ <35316562+JUAREZZ@users.noreply.github.com>
2018-04-01 19:05:37 -0500

    Update chapter11.sh

--- chapter11.sh
+++ chapter11.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+#week12 shells script
+# Juan Angel Juarez
 
+if [ $1 = week12 ]; then 
+   echo 'The first argument was "week12"'
+else
+   echo -n 'The first argument was not "week12" -- '
+   echo it was '"'$1'"'
+fi
