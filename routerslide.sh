451629a71067ec2e54e96d37439371b2091ca5d9
JUAREZZ <35316562+JUAREZZ@users.noreply.github.com>
2018-04-01 21:31:18 -0500

    Create routerslide.sh

---
+++ routerslide.sh
@@ -0,0 +1,29 @@
+# Week 12
+# juan Angel
+#Router Slide Article
+
+#knowing the basic of the IP and its meaning, like it say in the Article " IP Routing is the process by
+
+which a host with multiple network connections decides where to deliver IP datagrams it has
+
+received" saying that the host can decide where he wants deliver something to a specific location.
+
+Each host has its own routing rules called a routing table in which it has three segments, each segment
+
+has its own descrition. like shown in the article you can use the command " user% cat /proc/net/route " to whcih 
+
+it will give you the following fields:
+
+the first one is " Destination Address"
+
+second is " The name to where its being delivered/routed "
+
+third is " IP address of another machine which will carry the datagram on its next step through the network "
+
+knowing the routing table would be helpful since you would know what each field stand for.
+
+Reading this article has explained how each line of command would work and how it works. giving more
+
+explanation on how or why its being used. explaining each step or command a little more, and telling how 
+
+the router works. 
